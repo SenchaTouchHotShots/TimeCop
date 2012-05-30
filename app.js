e3c675df7a34b91ccfde6642ad7a83c1ec197a2b
John E. Clark <john@12ftguru.com>
2012-05-30 14:31:49 -0400

    new command friendly format

---
+++ app.js
@@ -0,0 +1,44 @@
+//<debug>
+Ext.Loader.setPath({
+    'Ext': 'sdk/src'
+});
+//</debug>
+
+Ext.application({
+    name: 'TimeCop3',
+
+    requires: [
+        'Ext.MessageBox',
+        'Ext.device.Notification'
+    ],
+
+    views: ['Main'],
+
+    icon: {
+        57: 'resources/icons/Icon.png',
+        72: 'resources/icons/Icon~ipad.png',
+        114: 'resources/icons/Icon@2x.png',
+        144: 'resources/icons/Icon~ipad@2x.png'
+    },
+
+    phoneStartupScreen: 'resources/loading/Homescreen.jpg',
+    tabletStartupScreen: 'resources/loading/Homescreen~ipad.jpg',
+
+    launch: function() {
+        // Destroy the #appLoadingIndicator element
+        Ext.fly('appLoadingIndicator').destroy();
+
+        // Initialize the main view
+        Ext.Viewport.add(Ext.create('TimeCop3.view.Main'));
+    },
+
+    onUpdated: function() {
+        Ext.Msg.confirm(
+            "Application Update",
+            "This application has just successfully been updated to the latest version. Reload now?",
+            function() {
+                window.location.reload();
+            }
+        );
+    }
+});
